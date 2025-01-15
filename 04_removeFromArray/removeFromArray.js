a7a2e5038f492782a472d934a321cf36113636a6
sarah-trip <177668675+sarah-trip@users.noreply.github.com>
2025-01-15 11:32:18 -0800

    Add function to remove items from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+// Remove items from an array 
+const removeFromArray = function(array, ...itemsToRemove) {
+    for (const item of itemsToRemove) { 
+        while (array.indexOf(item) != -1) {
+            array.splice(array.indexOf(item), 1);
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
