b35456c15cb186dd5f8475867557be7cad1c34bd
sarah-trip <177668675+sarah-trip@users.noreply.github.com>
2025-01-14 16:00:37 -0800

    Update repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+// Repeats a string a given number of non-negative times 
+const repeatString = function(string, num) {
+    if (num < 0) { 
+        return "ERROR";
+    }
+    let repeatedString = '';
+    for (let i = 0; i < num; i++) { 
+        repeatedString += string; 
+    }
+    return repeatedString; 
+}; 
 
 // Do not edit below this line
 module.exports = repeatString;
