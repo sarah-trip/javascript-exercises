6fb3fc0ac20ec729387d560eb4fa346bd434361f
sarah-trip <177668675+sarah-trip@users.noreply.github.com>
2025-01-15 13:37:21 -0800

    Add function to sum all numbers within a range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+// Sum all numbers between and including a min and max number
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) return "ERROR";
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    let sum = 0; 
+    if (max < min) {
+        const temp = min; 
+        min = max; 
+        max = temp; 
+    }
+    for (let i = min; i <= max; i++) {
+        sum += i; 
+    }
+    return sum; 
 };
 
 // Do not edit below this line
