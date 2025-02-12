6126022a3c64a557b848314321e3336e116aa1c8
sarah-trip <177668675+sarah-trip@users.noreply.github.com>
2025-02-11 18:01:06 -0800

    Add function to return a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (typeof number !== 'number') {
+        number = parseInt(number); 
+    }
+    if (number < 0) return 'OOPS'; 
+    let fibonacciSequence = [0, 1]; 
+    for (let i = 2; i <= number; i++) {
+        fibonacciSequence[i] = fibonacciSequence[i-2] + fibonacciSequence[i-1]; 
+    }
+    return fibonacciSequence[number]; 
 };
 
 // Do not edit below this line
