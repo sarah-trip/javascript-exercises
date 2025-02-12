14f9a9b36e6f137d83d7b7774ec69679622761d9
sarah-trip <177668675+sarah-trip@users.noreply.github.com>
2025-02-11 17:24:04 -0800

    Add function to check if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const allowedChars = 'abcdefghijklmnopqrstuvwxyz0123456789'.split(''); 
+    const updatedString = string
+        .toLowerCase()
+        .split('')
+        .filter(char => allowedChars.includes(char))
+        .join(''); 
+    const reversedString = updatedString
+        .split('')
+        .reverse()
+        .join(''); 
+    return updatedString === reversedString; 
 };
 
 // Do not edit below this line
