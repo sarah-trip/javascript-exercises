4c55977ea80da589643da00e74d5a332cc8910ce
sarah-trip <177668675+sarah-trip@users.noreply.github.com>
2025-02-11 16:25:27 -0800

    Add all calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2; 
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2; 
 };
 
-const sum = function() {
-	
+const sum = function(numbersArray) {
+	return numbersArray.reduce((sum, currentNumber) => sum + currentNumber, 0); 
 };
 
-const multiply = function() {
-
+const multiply = function(numbersArray) {
+  return numbersArray.reduce((total, currentNumber) => total * currentNumber, 1); 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2; 
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1; 
+  for (let i = num; i >= 2; i--){
+    total *= i; 
+  }
+  return total; 
 };
 
 // Do not edit below this line
